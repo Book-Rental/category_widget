213b21253f191674b5ac818a47f771025c1cc680
sunayana-lakki <sunayana.lakki@techdenali.com>
2026-07-02 17:27:07 +0530

    changes in ci.yml

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/category_widget/",
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.widget.tsx"),
