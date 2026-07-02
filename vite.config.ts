b83dbfde1bc1a633364969ffe52eb6679c63f526
sunayana-lakki <sunayana.lakki@techdenali.com>
2026-07-02 11:20:57 +0530

    resloving ci-cd error

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/category_widget/",
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.widget.tsx"),
